8aa1be209f693f323651f8659adc812faf5dffd7
yuanzf <yuanzf@connect.hku.hk>
2024-07-17 10:35:35 +0800

    update

---
+++ _script/d-experiment/c2_generate_tnse.py
@@ -0,0 +1,104 @@
+import os
+import pandas as pd
+import numpy as np
+from glob import glob
+import gspread
+import h3
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+from sklearn import manifold
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import NMF
+from sklearn.metrics import silhouette_score, silhouette_samples
+import seaborn as sns
+
+
+def get_std(df_seg_update, variables_remain):
+    scaler = StandardScaler().fit(df_seg_update[variables_remain])
+    data = scaler.transform(df_seg_update[variables_remain])
+    return data
+
+
+def get_tsne(data, n_components=2):
+
+    tsne = manifold.TSNE(n_components=n_components, random_state=0)
+    tsne_data = tsne.fit_transform(data)
+    return tsne_data
+
+ROOTFOLDER = "/lustre1/g/geog_pyloo/05_timemachine"
+DATA_FOLDER = f"{ROOTFOLDER}/_curated/c_seg_hex"
+FILES = os.listdir(DATA_FOLDER)
+
+FILENAME = "c_seg_cat=31_res={res}.parquet"
+FILENAME_WITHIN = "c_seg_cat=31_res={res}_withincity.parquet"
+
+variables = [
+    "bike",
+    "building",
+    "bus",
+    "car",
+    "grass",
+    "house",
+    "installation",
+    "lake+waterboday",
+    "light",
+    "mountain+hill",
+    "other",
+    "person",
+    "pole",
+    "railing",
+    "road",
+    "shrub",
+    "sidewalk",
+    "signage",
+    "sky",
+    "sportsfield",
+    "table+chair",
+    "tower",
+    "traffic light",
+    "trashcan",
+    "tree",
+    "truck",
+    "van",
+    "wall",
+    "window",
+]
+index_cols = ["city_lower", "hex_id", "img_count", "res"]
+
+
+def export_tnse(res, filename, variables=variables):
+
+    file_name = filename.format(res=res)
+    df = pd.read_parquet(os.path.join(DATA_FOLDER, file_name))
+    # standardize the data
+
+    data = get_std(df, variables)
+    print("finish basic standardization")
+    # get tsne
+    tsne_data = get_tsne(data)  # this takes very long time.
+    print("finish tsne")
+    # save the tsne_data once done
+    tsne_df = pd.DataFrame(tsne_data, columns=["tsne_1", "tsne_2"]).reset_index(
+        drop=True
+    )
+    print(tsne_df.head())
+    tsne_df = pd.concat([df[index_cols], tsne_df], axis=1)
+    print("finished concatination.")
+    print(tsne_df.head())
+
+    tsne_df.to_parquet(
+        os.path.join(
+            DATA_FOLDER,
+            file_name.replace(".parquet", "_tsne.parquet"),
+        ),
+        index=False,
+    )
+
+    return tsne_df
+
+
+for res in [8, 9, 12]:
+    for filename in [FILENAME_WITHIN, FILENAME]:
+        export_tnse(res=res, filename=filename)
+        print(f"finish {res} {filename}")
